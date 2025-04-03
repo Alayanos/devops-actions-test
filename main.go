9a29d51a72d62df91caa755a22dcb79a6a0f926d
Alayanos <104141296+Alayanos@users.noreply.github.com>
2025-04-03 11:00:15 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from linter test") // minor change to trigger CI
+	fmt.Println("Hello from linter test") // add comment to check the workflow
 }
